38f7c7af7fce7d4897813ac0365fe53ee84e8207
Elen <elen.souza12@etec.sp.gov.br>
2023-05-19 08:25:54 -0300

    view da enquete
    questaos, resultados e votos

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -5,5 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('musica', views.caneta, name='caneta'),
+    path('felipe', views.caneta, name='caneta'),
+    path("<int:questao_id>/", views.detalhe, name="detail"),
+    # ex: /enquete/5/
+    path("<int:questao_id>/resultados/", views.resultados, name="results"),
+    # ex: /enquete/5/resultados/
+    path("<int:questao_id>/voto/", views.voto, name="voto"),
+    # ex: /enquete/5/voto/
 ]
--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -7,4 +7,14 @@ def index(request):
     return HttpResponse("<h1>Olha, se você não me ama</h1><h2>Caneta azul, azul caneta</h2>")
 
 def caneta(request):
-    return HttpResponse("<h1>Olha, se você não me ama</h1><h2>Caneta azul</h2>")
+    return HttpResponse("<h1>Caneta azul, azul caneta...</h1>")
+
+def detalhe(request, questao_id):
+    return HttpResponse("Você está olhando a questão %s." % questao_id)
+
+def resultados(request, questao_id):
+    response = "Você está olhando o resultado da questão %s."
+    return HttpResponse(response % questao_id)
+
+def voto(request, questao_id):
+    return HttpResponse("Você está votando na questão %s." % questao_id)
