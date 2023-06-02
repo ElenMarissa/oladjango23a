46f7625e96d1b8e0707998ba1e0365a1293480c2
ElenMarissa <elen.souza12@etec.sp.gov.br>
2023-06-02 08:51:22 -0300

    erro
    erro

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Questao
 
 # Create your views here.
 
-# explicar isso semana que vem
 def index(request):
+    # selecionar na tabela questoes ultimos  5 objetos cadastrados
     ultimas_questoes = Questao.objects.order_by("data")[:5]
-    # saida = ",".join([q.pergunta for q in ultimas_questoes])
-    # saida = ""
-    # for q in ultimas_questoes:
-    #     linha = f"<p>{q.pergunta}</p>"
-    #     saida = saida + linha
+    # criamos um dicionario em python
+    # (semelhante aos arrays associativos do php)
+    # (ou aos arrays objetos literais do javascript)
+    # (onde passamos essa variavel paraser utilizada no templete)
     context = {'ultimas_questoes': ultimas_questoes}
     return render(request, 'enquete/index.html', context)
-
+    # retornar a função render, passando como argumentos
+    # a requisição, otemplete queserá utilizado
+    # e as variáveis de contextoque serão utilizadas dentro do templete
 
 def caneta(request):
     return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
 
 
 def detalhe(request, questao_id):
-    return HttpResponse("Voce esta olhando a questao %s." % questao_id)
+    try:
+        question = Questao.objects.get(pk=questao_id)
+    except Questao.DoesNotExist:
+        raise Http404("Questão não existe")
+    return render(repeat)
 
 
 def resultados(request, questao_id):
